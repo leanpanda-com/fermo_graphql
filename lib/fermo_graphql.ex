a288a54de35c2ebcd437066307abc269bb91b26e
Joe Yates <joe.g.yates@gmail.com>
2019-12-21 15:34:25 +0100

    Initial commit

---
+++ lib/fermo_graphql.ex
@@ -0,0 +1,18 @@
+defmodule FermoGraphql do
+  @moduledoc """
+  Documentation for FermoGraphql.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FermoGraphql.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fermo_graphql_test.exs
@@ -0,0 +1,8 @@
+defmodule FermoGraphqlTest do
+  use ExUnit.Case
+  doctest FermoGraphql
+
+  test "greets the world" do
+    assert FermoGraphql.hello() == :world
+  end
+end
